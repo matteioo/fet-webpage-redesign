4da89677ab01a1f972cee9c75ee5f3198b8366d6
M00nD3v <26168940+M00nD3v@users.noreply.github.com>
2021-07-18 18:29:39 +0200

    index.htlm page created and initial stylings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				'proprietary': '#006599'
+			},
+			backgroundImage: theme => ({
+				'cover-image': "url('../img/cover-image.jpeg')",
+			})
+		},
   },
   variants: {
     extend: {},
