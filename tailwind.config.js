bff915baeafc124093d9aec025119b2b64f71341
M00nD3v <26168940+M00nD3v@users.noreply.github.com>
2021-07-20 02:09:41 +0200

    added semi transparent gray-500 color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+		fontFamily: {
+			'sans': ['Poppins', ...defaultTheme.fontFamily.sans]
+		},
     extend: {
 			colors: {
-				'proprietary': '#006599'
+				'proprietary': '#006599',
+				'gray-transparent': '#6B7280A7'
 			},
 			backgroundImage: theme => ({
 				'cover-image': "url('../img/cover-image.jpeg')",
-			})
+			}),
 		},
   },
   variants: {
