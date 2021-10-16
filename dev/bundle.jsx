aa63103c1f6bc8907ebcb61c47d0b10c07858391
M00nD3v <26168940+M00nD3v@users.noreply.github.com>
2021-10-16 12:27:53 +0200

    bundle alpinejs and toolkit-screen magic helper

---
+++ dev/bundle.jsx
@@ -0,0 +1,9 @@
+/* *****Alpine.js*****
+	Import Alpine.js plugins before Alpine.start()!
+*/
+import Alpine from 'alpinejs'
+import Toolkit from '@alpine-collective/toolkit-screen'
+
+Alpine.plugin(Toolkit)
+
+Alpine.start()
