bdc93b53d2f28991326dde1c8897e18b7789c944
Matteioo <26168940+matteioo@users.noreply.github.com>
2023-08-03 12:26:17 +0200

    Transferring image links to another service as the old one is no longer available.

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -31,10 +31,10 @@ function cookieSet(cname) {
 	let ca = decodedCookie.split(';');
 	for(let i = 0; i < ca.length; i++) {
 		let c = ca[i];
-		while (c.charAt(0) == ' ') {
+		while (c.charAt(0) === ' ') {
 			c = c.substring(1);
 		}
-		if (c.indexOf(name) == 0) {
+		if (c.indexOf(name) === 0) {
 			return true;
 		}
 	}
--- dev/bundle.jsx
+++ dev/bundle.jsx
@@ -1,14 +1,113 @@
 /* *****Alpine.js*****
 	Import Alpine.js plugins before Alpine.start()!
 */
-import Alpine from 'alpinejs'
+import Alpine from '@alpinejs/csp'
 import Toolkit from '@alpine-collective/toolkit-screen'
 import collapse from '@alpinejs/collapse'
-import persist from '@alpinejs/persist'
 
+import persist from '@alpinejs/persist'
 Alpine.plugin(Toolkit)
 Alpine.plugin(collapse)
 Alpine.plugin(persist)
+
+Alpine.data('counter', () => ({
+	count: 1,
+
+	increment() { this.count++ }
+}))
+
+Alpine.data('search', () => ({
+	showSearch: false,
+	closeSearch() {
+		this.showSearch = false
+	},
+	openSearch() {
+		this.showSearch = true
+	},
+	toggleSearch() {
+		this.showSearch = ! this.showSearch
+	},
+	searchDocumentBody: {
+		['@keyup.escape']() {
+			this.showSearch = false
+		},
+		':class'() {
+			if (this.showSearch) {
+				return 'overflow-y-hidden';
+			}
+		}
+	}
+}))
+
+Alpine.data('navBar', () => ({
+	showNavBar: false,
+	toggleNav() {
+		this.showNavBar = ! this.showNavBar
+	},
+	navBarContent: {
+		['x-show']() {
+			return this.showNavBar || this.$screen('md')
+		}
+	}
+}))
+
+Alpine.data('pinnedPost', () => ({
+	pin: false,
+	showPin() {
+		this.pin = true
+	},
+	hidePin() {
+		this.pin = false
+	}
+}))
+
+Alpine.data('discordInfoBox', () => ({
+	showBox: true,
+	hideInfoBox() {
+		this.showBox = false
+	}
+}))
+
+Alpine.data('footerCounter', () => ({
+	counter: 0,
+	increase() {
+		this.counter++
+	},
+	footerFirst: {
+		':class'() {
+			if (this.counter >= 4) {
+				return 'text-purple-300'
+			}
+		}
+	},
+	footerSecond: {
+		':class'() {
+			if (this.counter >= 5)
+				return 'text-green-400'
+		}
+	},
+	footerThird: {
+		':class'() {
+			if (this.counter >= 2)
+				return 'text-proprietary hover:text-proprietary-dark'
+			else
+				return 'text-red-600 hover:text-red-700'
+		}
+	},
+	footerFourth: {
+		':class'() {
+			if (this.counter >= 6)
+				return 'text-pink-400'
+		}
+	},
+	footerFifth: {
+		':class'() {
+			if (this.counter >= 3)
+				return 'text-yellow-500'
+		}
+	}
+}))
+
 Alpine.start()
 
 /* *****Prism.js***** */
