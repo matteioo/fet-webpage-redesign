a105c5db360bda131620eb4a07a81e42bd39858b
Matteioo <26168940+matteioo@users.noreply.github.com>
2023-08-04 13:19:28 +0200

    Including alpine dependency as cloned project but not tracking any changes inside that folder.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Update the Alpine submodule
-git submodule init
-git submodule update --remote
+# Clone Alpine repository (if not already cloned)
+if [ ! -d "alpine" ]; then
+  git clone https://github.com/alpinejs/alpine.git alpine
+fi
 
-# Reference: https://github.com/alpinejs/alpine/discussions/1944#discussioncomment-2065588
-cd alpine || exit
+# Fetch the latest changes from Alpine repository
+cd alpine && git pull origin main
+
+# Install project dependencies
 npm install
 npm run build
-cd packages/csp || exit
-npm pack --loglevel notice 2>&1 >/dev/null | sed -e 's/^npm notice //'
 
+echo "*" >> .gitignore
+
+# Repack the tarball
+cd packages/csp && npm pack --loglevel notice 2>&1 >/dev/null | sed -e 's/^npm notice //'
+
+# Reference: https://github.com/alpinejs/alpine/discussions/1944#discussioncomment-2065588
+
+# Update base project
+npm update
