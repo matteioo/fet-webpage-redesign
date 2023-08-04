4389a9f6efadf15750f19dddb518c97ed1060180
Matteioo <26168940+matteioo@users.noreply.github.com>
2023-08-04 03:13:52 +0200

    Updated dependencies and fixed background overlay gradient. Included alpine as a git submodule and added update as well as postinstall scripts to this repo. Local tarball of alpine csp is needed and therefore automatically built when running npm install and npm run update.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update the Alpine submodule
+git submodule update --remote
+
+# Reference: https://github.com/alpinejs/alpine/discussions/1944#discussioncomment-2065588
+cd alpine || exit
+npm install
+npm run build
+cd packages/csp || exit
+npm pack --loglevel notice 2>&1 >/dev/null | sed -e 's/^npm notice //'
+
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run the setup.sh script
+./setup.sh
+
+# Update the npm packages
+npm update
